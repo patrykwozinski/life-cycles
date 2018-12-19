ad6f9eae9cf810340e34e884f5d7d35f534061d6
Patryk Woziński <patryk.wozinski@gmail.com>
2018-12-19 19:05:30 +0100

    Retrier init

---
+++ src/LifeCycle/RetrierLifeCycle.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: freeq
+ * Date: 19/12/2018
+ * Time: 19:05
+ */
+declare(strict_types=1);
+
+namespace Freeq\LifeCycle;
+
+
+final class RetrierLifeCycle implements LifeCycleInterface
+{
+    public function run(callable $app)
+    {
+        // TODO: Implement run() method.
+    }
+}
