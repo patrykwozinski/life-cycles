4d212bd2aad08cf78b51a48dd16ba06db45bbaf6
Patryk Woziński <patryk.wozinski@gmail.com>
2018-12-19 18:56:06 +0100

    Namespaces

---
+++ src/LifeCycle/InvokeLifeCycle.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: freeq
+ * Date: 19/12/2018
+ * Time: 18:54
+ */
+declare(strict_types=1);
+
+namespace Freeq\LifeCycle;
+
+
+final class InvokeLifeCycle
+{
+}
---
+++ tests/LifeCycle/TestCase.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: freeq
+ * Date: 19/12/2018
+ * Time: 18:55
+ */
+declare(strict_types=1);
+
+namespace Tests\Freeq\LifeCycle;
+
+
+use PHPUnit\Framework\TestCase as OriginalTestCase;
+
+abstract class TestCase extends OriginalTestCase
+{
+}
