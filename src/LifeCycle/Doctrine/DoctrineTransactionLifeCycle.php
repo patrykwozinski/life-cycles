a36992d19e86f4b26178fc960a789954d6928d0a
Patryk Woziński <patryk.wozinski@docplanner.com>
2018-12-20 12:48:48 +0100

    DoctrineTransactionLifeCycle

---
+++ src/LifeCycle/Doctrine/DoctrineTransactionLifeCycle.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: patryk.wozinski
+ * Date: 20/12/2018
+ * Time: 12:45
+ */
+declare(strict_types=1);
+
+namespace Freeq\LifeCycle\Doctrine;
+
+
+use Freeq\LifeCycle\LifeCycleInterface;
+use Doctrine\DBAL\Connection;
+
+final class DoctrineTransactionLifeCycle implements LifeCycleInterface
+{
+	/** @var LifeCycleInterface */
+	private $lifeCycle;
+
+	/** @var Connection */
+	private $connection;
+
+	public function __construct(LifeCycleInterface $lifeCycle, Connection $connection)
+	{
+		$this->lifeCycle  = $lifeCycle;
+		$this->connection = $connection;
+	}
+
+	public function run(callable $app)
+	{
+		$this->connection->beginTransaction();
+
+		try
+		{
+			$response = $this->lifeCycle->run($app);
+
+			$this->connection->commit();
+
+			return $response;
+		}
+		catch (\Exception $exception)
+		{
+			$this->connection->rollBack();
+		}
+	}
+}
