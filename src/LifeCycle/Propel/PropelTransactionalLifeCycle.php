1afbb208cefc1b0a97dc9a03a32117dd55ca0a15
Patryk Woziński <patryk.wozinski@docplanner.com>
2018-12-20 16:24:50 +0100

    PropelTransactionalLifeCycle

---
+++ src/LifeCycle/Propel/PropelTransactionalLifeCycle.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: patryk.wozinski
+ * Date: 20/12/2018
+ * Time: 16:22
+ */
+declare(strict_types=1);
+
+namespace Freeq\LifeCycle\Propel;
+
+
+use Freeq\LifeCycle\LifeCycleInterface;
+use Propel\Runtime\Propel;
+
+final class PropelTransactionalLifeCycle
+{
+	/** @var LifeCycleInterface */
+	private $lifeCycle;
+
+	/** @var Connection */
+	private $connection;
+
+	public function __construct(LifeCycleInterface $lifeCycle, string $connectionName)
+	{
+		$this->lifeCycle  = $lifeCycle;
+		$this->connection = Propel::getConnection($connectionName);
+	}
+
+	public function run(callable $app)
+	{
+		$this->connection->beginTransaction();
+
+		try
+		{
+			$response = $this->lifeCycle->run($app);
+
+			$this->connection->commit();
+
+			return $response;
+		}
+		catch (\Exception $exception)
+		{
+			$this->connection->rollBack();
+
+			throw $exception;
+		}
+	}
+}
